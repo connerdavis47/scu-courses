6237da3e8715c8a01cd7b3bef0594c603466a191
Conner Davis <connergdavis@gmail.com>
2019-03-21 21:28:02 -0700

    state system controls which part of form displays when, plus navigation back and forth

--- client/ui/page/Home.jsx
+++ client/ui/page/Home.jsx
@@ -76,6 +76,32 @@ const UGRAD_COEN = [
   'Educational Enrichment',
 ];
 
+const FormState = Object.freeze({
+  
+  /**
+   * The user is currently entering their degree program.
+   */
+  DEGREE: 'degree',
+  
+  /**
+   * The user is currently selecting University Core courses they have already
+   * satisfied.
+   */
+  CORE: 'core',
+  
+  /**
+   * The user is currently selecting completed classes within their major.
+   */
+  MAJOR: 'major',
+  
+  /**
+   * The user is possibly choosing extra filters, such as the desired earliest
+   * class time or chunks of time when they cannot take classes.
+   */
+  EXTRA: 'extra',
+  
+});
+
 /**
  * Landing page, which prompts users for basic degree information before
  * attempting to generate schedules.
@@ -86,43 +112,43 @@ class Home extends Component {
     super(props);
     
     this.state = {
-      
+  
       /**
-       * Whether a degree has been chosen from the dropdown, `#inputDegree`.
+       * The current active form state, which controls what should be displayed
+       * within the form. We use this primarily because the whole form is
+       * arguably information overload and rightly should be split into chunks.
        */
-      degreeSelected: false,
-      
+      formState: FormState.DEGREE,
+  
     };
   }
   
   render() {
     return (
-      <section id="home-form" className="home-form">
+      <section id="home-degree-form" className="home-degree-form">
         <h2 className="mb-4">Tell us a little about yourself.</h2>
         <Form className="border p-4">
-          <FormGroup>
-            <Label for="inputDegree">Degree Program</Label>
-            <Input type="select" name="degree" id="inputDegree"
-                   onChange={ this.toggleDegreeSelected }>
-              { Object.values(Degrees).map((degree, i) =>
-                <option key={ i } name={ degree }>{ degree }</option>
-              )}
-            </Input>
-          </FormGroup>
-          { this.state.degreeSelected &&
-            <FormGroup>
-              <Label>Select classes you've already taken.</Label>
-              <div className="home-form-courses d-flex flex-wrap">
-                { Object.values(UGRAD_COEN).map((course, i) =>
-                  <Badge key={ i } color="light" className="m-1 p-2 border"
-                         onClick={ this.toggleOneCourse }>
-                    { ` ${course}` }
-                  </Badge>
-                )}
-              </div>
-            </FormGroup>
-          }
-          { this.state.degreeSelected &&
+          { (() => {
+            /*
+              Display only one form group at a time to help user deal with
+              information overload.
+             */
+            switch (this.state.formState) {
+              case 'degree':
+                return this.renderFormDegree();
+                
+              case 'core':
+                return this.renderFormCore();
+                
+              case 'major':
+                return this.renderFormMajor();
+                
+              case 'extra':
+                return this.renderFormExtra();
+            }
+          })() }
+          { /* display submit once on the last stage */
+            this.state.formState === FormState.EXTRA &&
             <div>
               <hr />
               <Button outline color="success">Suggest Schedules</Button>
@@ -133,12 +159,76 @@ class Home extends Component {
     )
   }
   
-  /**
-   * @public
-   * Once the user has selected their degree, update state so the next stage of
-   * the form will roll out.
-   */
-  toggleDegreeSelected = () => this.setState({ degreeSelected: true, });
+  renderFormNav() {
+    return (
+      <div className="d-flex justify-content-between mt-4">
+        <Button outline color="danger"
+                onClick={ () => { this.updateFormState(false) } }>
+          <i className="fas fa-arrow-left pr-2"></i> Go back
+        </Button>
+        <Button outline color="success"
+                onClick={ () => { this.updateFormState(true) } }>
+          Next <i className="fas fa-arrow-right pl-2"></i>
+        </Button>
+      </div>
+    )
+  }
+  
+  renderFormDegree() {
+    return (
+      <FormGroup>
+        <Label for="inputDegree">Degree Program</Label>
+        <Input type="select" name="degree" id="inputDegree"
+               onChange={ () => { this.updateFormState(true) } }>
+          { Object.values(Degrees).map((degree, i) =>
+            <option key={ i } name={ degree }>{ degree }</option>
+          )}
+        </Input>
+      </FormGroup>
+    );
+  }
+  
+  renderFormCore() {
+    return (
+      <FormGroup>
+        <Label>Select University Core you've completed.</Label>
+        <div className="d-flex flex-wrap">
+          { Object.values(UGRAD_CORE).map((course, i) =>
+            <Badge key={ `C${i}` } color="light" className="m-1 p-2 border"
+                   onClick={ this.toggleOneCourse }>
+              { ` ${course}` }
+            </Badge>
+          )}
+        </div>
+        { this.renderFormNav() }
+      </FormGroup>
+    );
+  }
+  
+  renderFormMajor() {
+    return (
+      <FormGroup>
+        <Label>And pick the degree courses you've already finished.</Label>
+        <div className="home-form-courses d-flex flex-wrap">
+          { Object.values(UGRAD_COEN).map((course, i) =>
+            <Badge key={ `M${i}` } color="light" className="m-1 p-2 border"
+                   onClick={ this.toggleOneCourse }>
+              { ` ${course}` }
+            </Badge>
+          )}
+        </div>
+        { this.renderFormNav() }
+      </FormGroup>
+    );
+  }
+  
+  renderFormExtra() {
+    return (
+      <FormGroup>
+        <Label>Finally, choose any convenience filters you'd also like to include.</Label>
+      </FormGroup>
+    );
+  }
   
   /**
    * @public
@@ -147,10 +237,34 @@ class Home extends Component {
    *
    * @param e The badge to toggle to gray or brand primary.
    */
-  toggleOneCourse( e ) {
+  toggleOneCourse = ( e ) => {
     const options = [ 'badge-primary', 'badge-light' ];
     options.forEach(color => e.target.classList.toggle(color));
-  }
+  };
+  
+  updateFormState = ( next ) => {
+    let to;
+    
+    switch (this.state.formState) {
+      case 'degree':
+        to = next ? FormState.CORE : FormState.DEGREE;
+        break;
+      
+      case 'core':
+        to = next ? FormState.MAJOR : FormState.DEGREE;
+        break;
+        
+      case 'major':
+        to = next ? FormState.EXTRA : FormState.CORE;
+        break;
+        
+      case 'extra':
+        to = next ? FormState.EXTRA : FormState.MAJOR;
+        break;
+    }
+    
+    this.setState({ formState: to, });
+  };
   
 }
 export default hot(module)(Home)
